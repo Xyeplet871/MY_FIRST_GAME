d2bc42755fce787602802ada3acbd925f9114b5a
Xyeplet871 <matfeilagunovich@gmail.com>
2026-02-09 20:25:33 +0300

    menu in game

--- my-second-game/menu.gd
+++ my-second-game/menu.gd
@@ -12,3 +12,8 @@ func _on_play_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://settings_menu.tscn")
