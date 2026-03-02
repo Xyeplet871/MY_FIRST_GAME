33724e967d5dec3c88031475987d88857e8441b7
Xyeplet871 <matfeilagunovich@gmail.com>
2026-03-02 18:45:37 +0300

    менюха апдейт

--- my-second-game/menu.gd
+++ my-second-game/menu.gd
@@ -2,18 +2,13 @@ extends Node2D
 
 
 
-
-
-
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
-
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 
-
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://settings_menu.tscn")
+	get_tree ().change_scene_to_file("res://settings_menu.tscn")
+
+
+func _on_start_pressed() -> void:
+	get_tree(). change_scene_to_file("res://level.tscn")
